1edb23e6de17da132d9672a21ac5c40c84ac16bd
Patryk Drozd <68166254+drozd324@users.noreply.github.com>
2024-05-08 11:36:43 +0100

    Delete code directory

--- code/scrap/temp2.py
+++
@@ -1,8 +0,0 @@
-def calculateTotalSum(strin, *arguments):
-    totalSum = 0
-    for number in arguments:
-        totalSum += number
-    print(strin, totalSum)
- 
-# function call
-calculateTotalSum("lol", 5, 4, 3, 2, 1)
